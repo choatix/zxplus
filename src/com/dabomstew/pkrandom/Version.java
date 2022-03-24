d9ad8f468729a12484df525c94012ccc260d3943
tom-overton <tom.overton@outlook.com>
2022-03-24 04:38:21 -0700

    Bump version for release

--- src/com/dabomstew/pkrandom/Version.java
+++ src/com/dabomstew/pkrandom/Version.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 public class Version {
     public static final int VERSION = 318; // Increment by 1 for new version. Updated for 4.4.0
-    public static final String VERSION_STRING = "4.4.0-dev";
+    public static final String VERSION_STRING = "4.4.0";
 
     public static final Map<Integer,String> oldVersions = setupVersionsMap();
 
